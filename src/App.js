dc7b2b18cca146109e463030e0701ea212a710f2
tunakaynar <tunakaynar411@gmail.com>
2023-03-16 15:07:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      
+      test123
+      <img src='/logo192.png' alt=''></img>
+      <img src={logo} alt=''></img>
+      
+    </div>
+  );
+}
+
+export default App;
